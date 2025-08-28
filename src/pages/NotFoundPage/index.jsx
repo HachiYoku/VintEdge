164f4f09eea2f5d4517285a54db3c435a9e90941
HachiYoku <6531503142@lamduan.mfu.ac.th>
2025-08-28 22:39:46 +0700

    errorpage updated

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Result, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>404</h1>
-      <p>Sorry, the page you're looking for doesn't exist.</p>
-    </div>
+    <Result
+    status="403"
+    title="403"
+    subTitle="Sorry, you are not authorized to access this page."
+    extra={<Button type="primary"><Link to='/'>Back Home</Link></Button>}
+    />
   );
 };
 
