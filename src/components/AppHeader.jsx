b1d2b9817cb17ddd74be43e13e34b34430c59161
revy <revy200311@gmail.com>
2025-09-03 22:57:28 +0630

    import

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Layout, Space, Input,Badge } from "antd";
+import { Layout, Space, Input, Badge } from "antd";
 import BurgerMenu from "./BurgerMenu";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { TbUserSquare } from "react-icons/tb";
 import { PiShoppingCartBold } from "react-icons/pi";
 import { useCart } from "../context/CartContext";
+import ThemeToggle from "./ThemeToggle";
 
 const { Header } = Layout;
 
@@ -28,7 +29,7 @@ const AppHeader = ({ products = [] }) => {
   const location = useLocation();
   const { cart } = useCart();
 
-   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   // Pre-fill search input when on /search
   const searchParams = new URLSearchParams(location.search);
